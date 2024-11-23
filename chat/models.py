ca6e0b47732fabf7ec049422a4914382a9534625
randomicon00 <20146907+randomicon00@users.noreply.github.com>
2024-11-23 10:01:33 -0500

    feat: add model management

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Message(models.Model):
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{str(self.content)[:50]}..."  # Short preview of the message
+
+
+class Room(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return f"{self.name}"

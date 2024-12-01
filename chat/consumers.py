3e25d4399d4d91466f2fc2a4d8e5a9e6ea5e15e0
randomicon00 <20146907+randomicon00@users.noreply.github.com>
2024-12-01 10:06:17 -0500

    feat: add consumers logic

---
+++ chat/consumers.py
@@ -0,0 +1,27 @@
+import json
+import random
+from channels.generic.websocket import AsyncWebsocketConsumer
+
+
+class ChatConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        await self.accept()
+
+    async def receive(self, text_data):
+        data = json.loads(text_data)
+        user_message = data.get("message", "")
+
+        # Echo user's message back
+        await self.send(json.dumps({"type": "user", "message": user_message}))
+
+        # BOT responses
+        bot_responses = [
+            "Hello there! How can I assist you?",
+            "I'm just a bot, but I try my best!",
+            "Interesting... tell me more!",
+            "That's a great point.",
+        ]
+        bot_message = random.choice(bot_responses)
+
+        # Simulate bot typing delay
+        await self.send(json.dumps({"type": "bot", "message": bot_message}))

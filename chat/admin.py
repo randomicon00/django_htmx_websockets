467e3fbc62b1f616e0382e1a37f4ce361e59838e
randomicon00 <20146907+randomicon00@users.noreply.github.com>
2024-12-04 08:26:25 -0500

    feature: add admin models display

--- chat/admin.py
+++ chat/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Message, Room
 
-# Register your models here.
+
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ("content", "timestamp")
+    search_fields = ("content",)
+    list_filter = ("timestamp",)
+    ordering = ("-timestamp",)
+
+
+class RoomAdmin(admin.ModelAdmin):
+    list_display = ("name", "description")
+    search_fields = ("name", "description")
+
+
+admin.site.register(Message, MessageAdmin)
+admin.site.register(Room, RoomAdmin)

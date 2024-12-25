8f4e7181a70af37fa8f1f864ccd5438c2f9fdd46
randomicon00 <20146907+randomicon00@users.noreply.github.com>
2024-12-25 08:45:10 -0500

    refactor: use decorators for admins instead

--- chat/admin.py
+++ chat/admin.py
@@ -2,16 +2,15 @@ from django.contrib import admin
 from .models import Message, Room
 
 
+@admin.register(Message)
 class MessageAdmin(admin.ModelAdmin):
     list_display = ("content", "timestamp")
     search_fields = ("content",)
     list_filter = ("timestamp",)
     ordering = ("-timestamp",)
 
+
+@admin.register(Room)
 class RoomAdmin(admin.ModelAdmin):
     list_display = ("name", "description")
     search_fields = ("name", "description")
-
-
-admin.site.register(Message, MessageAdmin)
-admin.site.register(Room, RoomAdmin)

ed100e177c7df1c3eecc80804e432d00bf0caaef
randomicon00 <20146907+randomicon00@users.noreply.github.com>
2025-04-04 07:59:36 -0400

    edit: added list filter to Room admin

--- chat/admin.py
+++ chat/admin.py
@@ -21,5 +21,6 @@ class MessageAdmin(admin.ModelAdmin):
 class RoomAdmin(admin.ModelAdmin):
     list_display = ("name", "description")
     search_fields = ("name", "description")
+    list_filter = ("timestamp", "room")
     ordering = ("name",)
     list_per_page = 50
